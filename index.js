f281c4f42c442c38abb57f10a20790032e54c1a9
Vivian Gomez <viviangomez0@gmail.com>
2021-09-29 22:26:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML=("Vivian is the champion")
+document.body.append(newHeader)
